cf0d63892ca51cdaeffb529f03d142c6615ea7b4
VictorLMgit <victorlimacar@gmail.com>
2022-03-08 17:03:50 -0300

    animate contact area

--- js/main.js
+++ js/main.js
@@ -153,6 +153,23 @@ $( document ).ready(function() {
         $('#experience1').parent().removeClass('list-item-decored')
         $('#experience1').parent().addClass('list-item')
       })
+
+    let contactOffset = $('#contact').offset();
+    // stop serve para a animação não carregar mais que uma vez
+    let stop_0 = 0;
+  
+    $(window).scroll(function (e) {
+  
+      let scroll = $(window).scrollTop();
+  
+      if(scroll > (contactOffset.top - 500) && stop_0 == 0) {
+        $("#contact").addClass('move');
+        $("#info").addClass('move');
+  
+        stop = 1;
+      }
+  
+    });
 });
 
 
